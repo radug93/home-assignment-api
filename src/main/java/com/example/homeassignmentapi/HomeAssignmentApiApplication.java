2cac72a2f2fcc4c98589390dde11665a9bd1afd7
gabrielradu <radu.gabi93@gmaiil.com>
2022-12-23 08:59:41 +0200

    spring boot added

---
+++ src/main/java/com/example/homeassignmentapi/HomeAssignmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeassignmentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAssignmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeAssignmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/homeassignmentapi/HomeAssignmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeassignmentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAssignmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
